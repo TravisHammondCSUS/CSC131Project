19c96a9bd6bff40b1c917689faed2e8a26a70c36
fahim <fahim@LAPTOP-J3TKPPCS>
2021-11-08 17:38:07 -0800

    Constructor, resetMap, and handleClientMovement

--- Combat Game/src/server/World.java
+++ Combat Game/src/server/World.java
@@ -1,5 +1,6 @@
 package server;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class World {
 	private ArrayList<Entity> entities;
@@ -7,15 +8,19 @@ public class World {
 	private int team1Score, team2Score;
 	
 	public World (char[][] map) {
-		
+		defaultMap = map;
+		resetMap();
 	}
 	
 	public void resetMap() {
-		
+		currentMap = new char[defaultMap.length][];
+		for(int i = 0; i < defaultMap.length; i++) {
+			currentMap[i] = Arrays.copyOf(defaultMap[i], defaultMap[i].length);
+		}
 	}
 	
-	public void handleClientMovement(Entity entity, Direction direction) {
-		
+	public void handleClientMovement(Entity entity, int dx, int dy) {
+		entity.move(dx, dy); //not handling coheision or out of boundce
 	}
 	
 	public void handleClientAttack() {
