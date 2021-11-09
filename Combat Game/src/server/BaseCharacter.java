57756cf8aac17bc3d813a03527f9476acea37041
fahim <fahim@LAPTOP-J3TKPPCS>
2021-11-08 18:04:41 -0800

    update

--- Combat Game/src/server/BaseCharacter.java
+++ Combat Game/src/server/BaseCharacter.java
@@ -1,5 +1,25 @@
 package server;
 
 public class BaseCharacter {
-
+	private String clientOwner;
+	private int team;
+	private double health, defense, attackDmg, attackRate, attackDistance;
+	
+	public BaseCharacter(String clientOwner, int team, double health, double defense, double attackDmg, double attackRate, double attackDistance) {
+		this.clientOwner = clientOwner;
+		this.team = team;
+		this.health = health;
+		this.defense = defense;
+		this.attackDmg = attackDmg;
+		this.attackRate = attackRate;
+		this.attackDistance = attackDistance;
+	}
+	
+	public int getTeam() {
+		return team;
+	}
+	
+	public boolean handleCollision(Entity entity) {
+		
+	}
 }
