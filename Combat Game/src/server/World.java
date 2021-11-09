2a36558251d2e5641d15115724560eb8cb0ef3ab
fahim <fahim@LAPTOP-J3TKPPCS>
2021-11-08 17:27:51 -0800

    Start structuring the class

--- Combat Game/src/server/World.java
+++ Combat Game/src/server/World.java
@@ -1,5 +1,34 @@
 package server;
+import java.util.ArrayList;
 
 public class World {
-
+	private ArrayList<Entity> entities;
+	private char [][] defaultMap, currentMap;
+	private int team1Score, team2Score;
+	
+	public World (char[][] map) {
+		
+	}
+	
+	public void resetMap() {
+		
+	}
+	
+	public void handleClientMovement(Entity entity, Direction direction) {
+		
+	}
+	
+	public void handleClientAttack() {
+		
+	}
+	
+	public int checkWon() {
+		
+	}
+	
+	public void getCurrentMap() {
+		
+	}
+	
 }
+
