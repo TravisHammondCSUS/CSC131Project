406935aaa3bf90b40ab42017e91ffd61f7927e3a
manueldiazdev <mhdiaz20@gmail.com>
2021-11-10 00:01:19 -0800

    Updated Entity class

--- Combat Game/src/server/Entity.java
+++ Combat Game/src/server/Entity.java
@@ -5,7 +5,7 @@ import java.awt.Point;
 public class Entity {
     protected char symbol;
     public static final String ENTITY_TYPE = "ENTITY";
-    private Point position;
+    protected Point position;
 
     public Entity(char symbol){
         this.symbol = symbol;
@@ -17,6 +17,18 @@ public class Entity {
 
     public boolean handleCollision(Entity entity){
         return true;
+    }
+    
+    public void setPosition(int x, int y){
+        this.position.setLocation(x, y);
+    }
+
+    public Point getPosition(){
+        return position;
+    }
+
+    public void move(int dx, int dy){
+        this.position.translate(dx, dy);
     }
 
 
