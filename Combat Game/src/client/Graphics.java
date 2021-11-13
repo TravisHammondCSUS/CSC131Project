104920d36ec7cc9271e7761bccb646cac400b8c3
BMYasir <Bmyasirarafat@csus.edu>
2021-11-13 08:42:56 -0800

    Graphics update

--- Combat Game/src/client/Graphics.java
+++ Combat Game/src/client/Graphics.java
@@ -1,5 +1,18 @@
 package client;
 
 public class Graphics {
+    public void updateConsole(char [][] world) {
+    	String worldStr = "";
+        for (int i = 0; i < world.length; i++) {
+          worldStr += new String(world[i]) + '\n';
+        }
+        System.out.print(worldStr);
 
+    }
+    public void clearConsole() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+             
+    }
+    
 }
