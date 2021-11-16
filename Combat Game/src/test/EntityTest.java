184bddc774bba3416debf2b5ee3c5ea4b21bed9e
manueldiazdev <manueldiazdev@gmail.com>
2021-11-15 19:09:07 -0800

    Init Junit 5

---
+++ Combat Game/src/test/EntityTest.java
@@ -0,0 +1,26 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.awt.Point;
+import org.junit.jupiter.api.Test;
+import server.Entity;
+
+
+
+class EntityTest {
+
+	@Test
+	void test() {
+		Entity test = new Entity('f', new Point(1,1));
+		Entity dummy = new Entity('d', new Point(0,0));
+		test.move(1, 1);
+		test.setPosition(2, 2);
+		Point testingP = new Point(2,2);
+		assertEquals(test.getPosition(), testingP);
+		assertEquals(test.getEntityType(), "ENTITY");
+		assertEquals(test.getSymbol(), 'f');
+		assertEquals(test.handleCollision(dummy), false);
+	}
+
+}
