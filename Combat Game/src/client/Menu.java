f3e3ad12ad4097d845b39084f14c926cfda925ca
mac <mac@10.0.0.4>
2021-11-15 20:04:18 -0800

    Added handleTeamMenu() and handleCharactersMenu()

--- Combat Game/src/client/Menu.java
+++ Combat Game/src/client/Menu.java
@@ -2,32 +2,29 @@ package client;
 
 import java.util.*;
 public class Menu {
-	public Menu() {
-	}
-	public int handleMainMenu() {
-		printMenu();
-		int in = input();
-		return in;
-	}
-	public void printMenu() {
-		System.out.println("-------------------------");
-		System.out.println("| WELCOME TO OUR GAME!! |");
-		System.out.println("-------------------------\n");
-		System.out.println("(1) JOIN GAME");
-		System.out.println("(2) QUIT");
-	}
-	public int input() {
-		Scanner kb = new Scanner(System.in);
-		int choice = -1;
-		while( choice < 1 || choice > 2) {
-			try {
-				System.out.println("\nPLEASE ENTER YOUR CHOICE");
-				choice = Integer.parseInt(kb.nextLine());
-			}
-			catch(NumberFormatException e) {
-				System.out.println("INVALID SELECTION, PLEASE TRY AGAIN.");
-			}
-		}
+
+   public Menu() {
+   
+   }
+   
+   public int handleMainMenu() {
+      Scanner kb = new Scanner(System.in);
+      System.out.println("-------------------------------");
+      System.out.println("|    Welcome To Our Game!!    |");
+      System.out.println("-------------------------------\n");
+      System.out.println("(1) Join Game");
+      System.out.println("(2) Quit");
+      int choice = -1;
+      while( choice < 1 || choice > 2) {
+         try {
+            System.out.print("\nEnter Your Choice: ");
+            choice = Integer.parseInt(kb.nextLine());
+            System.out.println();
+         }
+         catch(NumberFormatException e) {
+            System.out.println("\nInvalid Selection Please Try Again.\n");
+         }
+      }
       if(choice == 1){
          return 1;
       }
@@ -35,5 +32,29 @@ public class Menu {
          return 2;
       }
       return 0;
-	}
+   }
+   
+   public int handleTeamMenu(){
+      Scanner kb = new Scanner(System.in);
+      System.out.println("Choose your Team:");
+      System.out.println("(1) Team 1");
+      System.out.println("(2) Team 2");
+      System.out.print("\nEnter Your Choice: ");
+      int team = kb.nextInt();
+      System.out.println();
+      return team;
+   }
+   
+   public String handleCharactersMenu(){
+      Scanner kb = new Scanner(System.in);
+      System.out.println("Choose your character:");
+      System.out.println("Archer");
+      System.out.println("Mage");
+      System.out.println("Rogue");
+      System.out.println("Warrior");
+      System.out.print("\nType out your Choice: ");
+      String character = kb.next();
+      System.out.println();
+      return character.toUpperCase();
+   } 
 }
