6925836cf2cfa95522c3ecd42311227827ac7cb9
TravisHammondCSUS <90869648+TravisHammondCSUS@users.noreply.github.com>
2021-11-19 15:26:40 -0800

    Fix formating for EntityFactory

--- Combat Game/src/server/EntityFactory.java
+++ Combat Game/src/server/EntityFactory.java
@@ -6,15 +6,22 @@ public class EntityFactory {
     public EntityFactory(){
     }
 
-    public Entity create(String entityType, char symbol, Point position, int ticksPerMovement, int team, double health, double defense, double attackDmg, int attackRate, int attackSpeed, double attackDistance){
+    public Entity create(String entityType, char symbol, Point position,
+    					 int ticksPerMovement, int team, double health,
+    					 double defense, double attackDmg, int attackRate,
+    					 int attackSpeed, double attackDistance){
         if(entityType.equals("BASE_CHARACTER")){
-            return new BaseCharacter(symbol, position, ticksPerMovement, team, health, defense, attackDmg, attackRate, attackSpeed, attackDistance);
+            return new BaseCharacter(symbol, position, ticksPerMovement,
+            						 team, health, defense, attackDmg,
+            						 attackRate, attackSpeed, attackDistance);
         }else{
             return null;
         }
         
     }
-    public Entity create(String entityType, char symbol, Point position, int team, double distance, double damage, int ticksPerMovement, int dx, int dy){
+    public Entity create(String entityType, char symbol, Point position,
+    					 int team, double distance, double damage,
+    					 int ticksPerMovement, int dx, int dy){
         if(entityType.equals("PROJECTILE")){
             return new Projectile(symbol, position, team, distance, damage, ticksPerMovement, dx, dy);
         }else{
